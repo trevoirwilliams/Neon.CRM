e69c8f1960705036ebb49ec9f38cf43921757d30
twilliams <trevoir.williams@gmail.com>
2025-04-09 11:39:36 -0500

    Add project files.

---
+++ Neon.CRM.WebApp/Pages/Shared/_AdminLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Neon.CRM.WebApp.Pages.Shared
+{
+    public class _AdminLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
